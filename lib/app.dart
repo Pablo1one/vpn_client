f1f295deaafc684bdcccd1eb07166b08f489284e
Pablo1one <kruglyackov2013@gmail.com>
2026-05-24 23:41:12 +0400

    добавить скорость соединения, иконку трея, проверку обновлений

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 import 'providers/vpn_provider.dart';
 import 'providers/language_provider.dart';
+import 'services/tray_service.dart';
+import 'services/vpn_service.dart';
 import 'screens/home_screen.dart';
 import 'screens/profiles_screen.dart';
 import 'screens/settings_screen.dart';
@@ -38,14 +42,56 @@ class _Shell extends StatefulWidget {
   State<_Shell> createState() => _ShellState();
 }
 
-class _ShellState extends State<_Shell> {
+class _ShellState extends State<_Shell> with WindowListener {
   int _index = 0;
+  final _tray = TrayService();
 
   static const _screens = [
     HomeScreen(),
     ProfilesScreen(),
     SettingsScreen(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    if (TrayService.isSupported) {
+      windowManager.addListener(this);
+      _initTray();
+    }
+  }
+
+  Future<void> _initTray() async {
+    final vpn = context.read<VpnProvider>();
+    await _tray.init(
+      onConnect: vpn.connect,
+      onDisconnect: vpn.disconnect,
+    );
+    vpn.addListener(_onVpnChanged);
+  }
+
+  void _onVpnChanged() {
+    final vpn = context.read<VpnProvider>();
+    _tray.updateStatus(vpn.status);
+  }
+
+  @override
+  void dispose() {
+    if (TrayService.isSupported) {
+      windowManager.removeListener(this);
+      context.read<VpnProvider>().removeListener(_onVpnChanged);
+      _tray.destroy();
+    }
+    super.dispose();
+  }
+
+  // Minimize to tray on close instead of quitting
+  @override
+  void onWindowClose() async {
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+      await windowManager.hide();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsDesktop) {
+    runApp(const App());
+    return;
+  }
+
+  await windowManager.ensureInitialized();
+  await windowManager.setTitle('VPN Client');
+  await windowManager.setMinimumSize(const Size(480, 600));
+  await windowManager.setPreventClose(true);
+
   runApp(const App());
 }
+
+// ignore: non_constant_identifier_names
+bool get kIsDesktop =>
+    !const bool.fromEnvironment('dart.library.js_util') &&
+    (Platform.isWindows || Platform.isMacOS || Platform.isLinux);
