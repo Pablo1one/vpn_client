922c2e849f832018816f0b6552b9d77ff90fec6b
Pablo1one <kruglyackov2013@gmail.com>
2026-05-25 14:14:14 +0400

    че попало

--- lib/utils/xray_config_builder.dart
+++ lib/utils/xray_config_builder.dart
@@ -25,9 +25,13 @@ class XrayConfigBuilder {
         {'tag': 'block', 'protocol': 'blackhole'},
       ],
       'routing': {
-        'domainStrategy': 'IPIfNonMatch',
+        'domainStrategy': 'AsIs',
         'rules': [
-          {'type': 'field', 'ip': ['geoip:private'], 'outboundTag': 'direct'},
+          {
+            'type': 'field',
+            'ip': ['127.0.0.0/8', '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '169.254.0.0/16'],
+            'outboundTag': 'direct',
+          },
         ],
       },
     };
